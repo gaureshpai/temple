a054cad3fca37118a6da08ac74297156d0985caf
paigauresh <paigauresh@gmail.com>
2024-09-14 20:16:52 +0530

    Home page with its components rendered

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+import HomeAboutSection from "@/components/HomeAboutSection";
+import HomeImageSlider from "@/components/HomeImageSlider";
+import HomeTrusteeTab from "@/components/HomeTrusteeTab";
+import LatestUpdates from "@/components/LatestUpdates";
+
 export default function Home() {
   return (
     <div>
-      
+      <HomeImageSlider/>
+      <HomeAboutSection/>
+      <HomeTrusteeTab/>
+      <LatestUpdates/>
     </div>
   );
 }
