18774b2ba35bb80fbb9e1fcbb3dbe774c142717c
paigauresh <paigauresh@gmail.com>
2024-09-14 23:50:08 +0530

    multiple changes for larger screens

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -25,99 +25,101 @@ const Navbar = () => {
   ];
 
   return (
-    <div className="bg-white">
-      <nav
-        className='fixed w-full top-0 left-0 z-50 transition-all duration-300 ease-in-outbg-white shadow-lgh-[15vh]'
-      >
+    <div className="bg-white overflow-hidden">
+      <nav className="fixed w-full top-0 left-0 z-50 transition-all duration-300 ease-in-out bg-white shadow-lg">
+        {/* Top Message Bar */}
         <div
-          className={`bg-orange-400 text-white p-2 text-center ${isScrolled ? 'hidden' : 'block'
+          className={`bg-orange-400 justify-center content-center text-white text-center text-sm lg:text-base 5xl:text-xl h-[3vh] ${isScrolled ? 'hidden' : 'block'
             }`}
         >
           Official website for the Rama temple
         </div>
-        <div className="bg-white">
-          <div className='max-w-6xl justify-between container mx-auto p-2 flex  items-center'>
-            {/* Logo on the left */}
-            <div className="flex items-center">
-              <Link href="/" className="text-xl font-bold text-orange-400">
-                <Image
-                  className="w-auto"
-                  src="/images/logo.jpg"
-                  alt="Logo"
-                  width={50}
-                  height={50}
-                />
-              </Link>
+
+        <div className="bg-white h-[12vh]">
+          <div className="container mx-auto h-full px-2 max-w-5xl flex items-center justify-between">
+            {/* Mobile and Desktop Layout */}
+            <div className="flex items-center w-full justify-between md:w-auto">
+              {/* Logo */}
+              <div className="flex items-center">
+                <Link href="/" className="text-xl 5xl:text-2xl font-bold text-orange-400 flex items-center">
+                  <Image
+                    className="w-auto"
+                    src="/images/logo.jpg"
+                    alt="Logo"
+                    width={50}
+                    height={50}
+                  />
+                </Link>
+              </div>
+
+              {/* Hamburger Menu (mobile view) */}
+              <div className="md:hidden flex items-center">
+                <button
+                  title="hamburger"
+                  onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+                  className="text-gray-700 focus:outline-none"
+                >
+                  <svg
+                    className="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d={
+                        isMobileMenuOpen
+                          ? 'M6 18L18 6M6 6l12 12'
+                          : 'M4 6h16M4 12h16M4 18h16'
+                      }
+                    />
+                  </svg>
+                </button>
+              </div>
             </div>
 
-            {/* Desktop Menu on the right */}
-            <div className="hidden md:flex items-center space-x-6">
+            {/* Desktop Menu */}
+            <div className="hidden md:flex items-center h-full space-x-6">
               {navItems.map((item) => (
                 <Link
                   key={item.name}
                   href={item.href}
-                  className="text-gray-700 hover:bg-orange-400 hover:text-white transition duration-300 p-2 rounded-lg"
+                  className="text-gray-700 hover:bg-orange-400 hover:text-white transition duration-300 p-2 rounded-lg text-base 5xl:text-xl 6xl:text-2xl"
                 >
                   {item.name}
                 </Link>
               ))}
-              <button className="bg-red-500 text-white px-6 py-2 rounded-3xl animate-pulse transition duration-300 hover:bg-red-600">
+              <button className="bg-red-500 text-white px-6 py-2 rounded-3xl animate-pulse transition duration-300 hover:bg-red-600 text-sm lg:text-base xl:text-lg 2xl:text-xl 6xl:text-2xl">
                 Live
               </button>
             </div>
           </div>
 
-
-          {/* Mobile Hamburger Menu */}
-          <div className="md:hidden">
-            <button
-              title="ham"
-              onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
-              className="text-gray-700 focus:outline-none"
-            >
-              <svg
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d={
-                    isMobileMenuOpen
-                      ? 'M6 18L18 6M6 6l12 12'
-                      : 'M4 6h16M4 12h16M4 18h16'
-                  }
-                />
-              </svg>
-            </button>
-          </div>
+          {/* Mobile Menu */}
+          {isMobileMenuOpen && (
+            <div className="md:hidden bg-white shadow-lg fixed top-[15vh] left-0 right-0 transition-all duration-300 ease-in-out">
+              <div className="flex flex-col px-6 py-6 space-y-4">
+                {navItems.map((item) => (
+                  <Link
+                    key={item.name}
+                    href={item.href}
+                    className="text-gray-700 hover:text-orange-400 transition duration-300 text-base lg:text-lg xl:text-xl 2xl:text-2xl"
+                    onClick={() => setIsMobileMenuOpen(false)}
+                  >
+                    {item.name}
+                  </Link>
+                ))}
+                <button className="bg-red-500 text-white px-4 py-2 rounded-3xl animate-pulse hover:bg-red-600 text-sm lg:text-base xl:text-lg 2xl:text-xl mt-2">
+                  Live
+                </button>
+              </div>
+            </div>
+          )}
         </div>
-
-        {/* Mobile Menu */}
-        {isMobileMenuOpen && (
-          <div className="md:hidden bg-white shadow-lg fixed top-[15vh] left-0 right-0 transition-all duration-300 ease-in-out">
-            <div className="flex flex-col px-4 py-4 space-y-4">
-              {navItems.map((item) => (
-                <Link
-                  key={item.name}
-                  href={item.href}
-                  className="text-gray-700 hover:text-orange-400 transition duration-300"
-                  onClick={() => setIsMobileMenuOpen(false)}
-                >
-                  {item.name}
-                </Link>
-              ))}
-              <button className="bg-red-500 text-white px-4 py-2 rounded-3xl animate-pulse mt-2 hover:bg-red-600">
-                Live
-              </button>
-            </div>
-          </div>
-        )}
       </nav>
-      <div className='min-h-[18vh]'></div>
+      <div className="h-[15vh]"></div>
     </div>
   );
 };
