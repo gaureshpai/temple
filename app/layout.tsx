8e8e8ae418a5f273fe96a97ac0aa8e8402aadafa
paigauresh <paigauresh@gmail.com>
2024-09-14 20:16:09 +0530

    fonts removed from layout and changed the layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,9 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import "@/public/styles/globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import SessionProvider from '@/app/SessionProvider'
 
 export const metadata: Metadata = {
   title: "temple website",
@@ -26,9 +18,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className= ''
       >
+        <SessionProvider>
+        <Navbar/>
         {children}
+        <Footer/>
+        </SessionProvider>
       </body>
     </html>
   );
