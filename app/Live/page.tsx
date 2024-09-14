6a963ae78a5f5d00e4dc4efcfbc4046f4f441f48
paigauresh <paigauresh@gmail.com>
2024-09-14 20:35:02 +0530

    Live page with its components rendered with it sub pages

--- app/Live/page.tsx
+++ app/Live/page.tsx
@@ -1,8 +1,11 @@
+import LiveEmbeddedYt from '@/components/LiveEmbeddedYt'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div>
+      <LiveEmbeddedYt/>
+    </div>
   )
 }
 
